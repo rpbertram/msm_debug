de42fe4feecabaea22c4d13530560322f7eeb601
Ryan Bertram <rpbertram@users.noreply.github.com>
2016-11-01 19:01:04 -0500

    Movie create and show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,9 +5,11 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
+    render("movies/show.html.erb")
   end
 
   def new_form
+    render("movies/new_form.html.erb")
   end
 
   def create_row
@@ -17,7 +19,7 @@ class MoviesController < ApplicationController
     @movie.duration = params[:the_duration]
     @movie.description = params[:the_description]
     @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie.director_id = params[:director_id]
 
     @movie.save
 
